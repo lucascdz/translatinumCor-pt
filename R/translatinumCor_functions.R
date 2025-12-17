9c62410f66f6e9bfc266243f948cfc6bfd763daa
Lucas Dezotti <lucascdz@gmail.com>
2025-12-17 14:16:43 -0300

    pipeline works fine

--- R/translatinumCor_functions.R
+++ R/translatinumCor_functions.R
@@ -2,12 +2,12 @@
 library(udpipe)
 library(tidyverse)
 
-#outputFolder <- '/../../FILES/atomiclab/corpora/UD_Latin-PROIEL/'
+#outputfolder <- '/../../FILES/atomiclab/corpora/UD_Latin-PROIEL/'
 #conlluFile <- '/../../FILES/atomiclab/corpora/UD_Latin-PROIEL/Hieronymus_Vulgata.conllu'
 
 GetSentenceMetadata <- function(corpusname){
    SentMetadataDF <- read.delim2(paste0('./data/sentMetadata_',corpusname,'.tsv'))
-   SentMetadataDF$sentence_id <- gsub('proiel_','',SentMetadataDF$sent_id)
+   SentMetadataDF$sentence_id <- as.character(SentMetadataDF$sentence_id)
    SentMetadataDF$filename <- gsub('\\d+.*','',SentMetadataDF$ref) %>%
       gsub('\\.','_',.) %>%
       gsub('(.*)',paste0('\\1',corpusname),.)
@@ -18,19 +18,19 @@ GetSentencesFromConllu <- function(conlluFile,corpusname,refstring,SentMetadataD
 
    conlluDF <- udpipe::udpipe_read_conllu(conlluFile)
    tokensDF <- left_join(conlluDF,SentMetadataDF)
-   SentencesDF <- tokensDF[str_detect(tokensDF$ref,refstring),colnames(tokensDF) %in% c('sentence_id','sentence','ref')]
+   SentencesDF <- tokensDF[str_detect(tokensDF$ref,refstring),colnames(tokensDF) %in% c('sentence_id','sentence','ref','sent_order')]
    SentencesDF <- SentencesDF[!duplicated(SentencesDF),]
 
-   outputFolder <- './data/sentences/'
-   system(paste0('mkdir ',outputFolder))
-   write_tsv(SentencesDF,paste0(outputFolder,gsub('\\.','_',refstring),'_sentences.tsv'))
+   outputfolder <- './data/0_sentences/'
+   system(paste0('mkdir ',outputfolder))
+   write_tsv(SentencesDF,paste0(outputfolder,gsub('\\.','_',refstring),'_',corpusname,'_sentences.tsv'))
 
    return(SentencesDF)
 
 }
 
 
-GetTranslationsFromDOC <- function(docfile,SentencesDF,refstring){
+GetTranslationsFromDOC <- function(docfile,corpusname,refstring){
 
    doctext <- readLines(textConnection(antiword(docfile)))
    doctext <- doctext[str_detect(doctext,'^\\|')]
@@ -54,51 +54,77 @@ GetTranslationsFromDOC <- function(docfile,SentencesDF,refstring){
    }
 
    TextTranslationDF <- data.frame(latintext=LatinText,
-                                  translation=Translation,
-                                  stringsAsFactors = F)
+                                   translation=Translation,
+                                   stringsAsFactors = F)
 
-   outputfolder <- './data/align2check/'
+   outputfolder <- './data/1_align2check/'
    system(paste0('mkdir ',outputfolder))
-   write_tsv(TextTranslationDF,paste0(outputfolder,gsub('\\.','_',refstring),'_align2check.tsv'))
+   write_tsv(TextTranslationDF,paste0(outputfolder,gsub('\\.','_',refstring),'_',corpusname,'_align2check.tsv'))
 
    return(TextTranslationDF)
 }
 
 
-CheckTranslationDiff <- function(){
+GetTranslationsFromMD <- function(mdfile,corpusname,refstring,translator){
+
+   mdtext <- readLines(mdfile)
+   mdtext <- mdtext[str_detect(mdtext,'^\\|')]
+   mdtext <- gsub('^\\|','',mdtext) %>%
+      gsub('\\|$','',.)
+
+   LatinText <- paste(gsub('(.*)\\|(.*)','\\1',mdtext)) %>%
+      gsub(' +',' ',.) %>%
+      gsub('^ ','',.) %>%
+      gsub(' $','',.)
+
+   Translation <- paste(gsub('(.*)\\|(.*)','\\2',mdtext)) %>%
+      gsub(' +',' ',.) %>%
+      gsub('^ ','',.) %>%
+      gsub(' $','',.)
+
+   TextTranslationDF <- data.frame(latintext=LatinText,
+                                   translation=Translation,
+                                   translator=translator,
+                                   stringsAsFactors = F)
+
+   outputfolder <- './data/1_align2check/'
+   system(paste0('mkdir ',outputfolder))
+   write_tsv(TextTranslationDF,paste0(outputfolder,gsub('\\.','_',refstring),'_',corpusname,'_align2check.tsv'))
+
+   return(TextTranslationDF)
+}
+
+
+CheckTranslationDiff <- function(alignedfile,refstring,corpusname){
 
    # check translation alignment
-   translatedSentencesDF <- read.csv('/Users/lucascdz/FILES/atomiclab/corpora/Proiel_UD/ProielSentTranslationsJDD.tsv',sep='\t')
-   translatedSentencesDF$sentence_id <- gsub('proiel_','',translatedSentencesDF$sent_id)
+   CheckTranslationDF <- read.csv(alignedfile,sep='\t')
+   CheckTranslationDF <- CheckTranslationDF[!is.na(CheckTranslationDF$sentence_id),]
+   CheckTranslationDF$norm_sentence <- gsub('[^A-ω ]', '', CheckTranslationDF$sentence) %>%
+      gsub(' +', ' ', .) %>%
+      gsub(' que ', 'que ', .) %>%
+      gsub(' c ', 'c ', .) %>%
+      gsub('si s ', 'sis ', .) %>%
+      gsub("(.*)", "\\L\\1", ., perl = T) %>%
+      gsub('j', 'i', .) %>%
+      gsub('v', 'u', .) %>%
+      gsub('^ ?(.*) ?$','\\1',.)
+   CheckTranslationDF$norm_text <- gsub('[^A-ω ]', '', CheckTranslationDF$latintext) %>%
+      gsub(' +', ' ', .) %>%
+      gsub(' que ', 'que ', .) %>%
+      gsub(' c ', 'c ', .) %>%
+      gsub('si s ', 'sis ', .) %>%
+      gsub("(.*)", "\\L\\1", ., perl = T) %>%
+      gsub('j', 'i', .) %>%
+      gsub('v', 'u', .) %>%
+      gsub('^ ?(.*) ?$','\\1',.)
+   CheckTranslationDF$sameEdition <- unlist(lapply(seq_along(CheckTranslationDF[,1]), function(i) CheckTranslationDF$norm_sentence[i]==CheckTranslationDF$norm_text[i]))
+   table(CheckTranslationDF$sameEdition)
 
-   checkTranslationAlignment <- left_join(VulgataMattDF[,colnames(VulgataMattDF) %in% c('sentence_id','sentence')],translatedSentencesDF)
-   checkTranslationAlignment <- checkTranslationAlignment[!duplicated(checkTranslationAlignment),]
-   write_tsv(checkTranslationAlignment,paste0(outputFolder,'VulgataMatt_sentences.tsv'))
+   outputfolder <- './data/3_diff2check/'
+   system(paste0('mkdir ',outputfolder))
+   write_tsv(CheckTranslationDF,paste0(outputfolder,gsub('\\.','_',refstring),'_',corpusname,'_diff2check.tsv'))
 
-   translationAlignment_raw <- read.csv('/Users/lucascdz/FILES/atomiclab/corpora/UD_Latin-PROIEL/VulgataMatt_translAlignment_raw.tsv',sep='\t')
-
-   CheckTranslation <- function(Filepath){
-      #Filepath <- '/Users/lucascdz/FILES/atomiclab/corpora/UD_Latin-PROIEL/VulgataMatt_translAlignment.tsv'
-      CorpusDF <- read.csv(Filepath,sep = '\t')
-      CorpusDF$norm_sentence <- gsub('\\d', '', CorpusDF$sentence) %>%
-         gsub("(.*)", "\\L\\1", ., perl = T) %>%
-         gsub('j', 'i', .) %>%
-         gsub('v', 'u', .) %>%
-         gsub('[\\.\\,\\?\\!\\:\\;\\)\\(“‘”’]', '', .) %>%
-         gsub(' que ', 'que ', .) %>%
-         gsub('  ', ' ', .) %>%
-         gsub(' $', '', .)
-      CorpusDF$norm_text <- gsub('\\d', '', CorpusDF$text) %>%
-         gsub("(.*)", "\\L\\1", ., perl = T) %>%
-         gsub('j', 'i', .) %>%
-         gsub('v', 'u', .) %>%
-         gsub('[\\.\\,\\?\\!\\:\\;\\)\\(“‘”’]', '', .) %>%
-         gsub(' +',' ',.) %>%
-         gsub('^ ','',.) %>%
-         gsub(' $','',.)
-      CorpusDF$sameText <- unlist(lapply(seq_along(CorpusDF[,1]), function(i) CorpusDF$norm_sentence[i]==CorpusDF$norm_text[i]))
-      write_tsv(CorpusDF,gsub('.tsv$', '_diff.tsv', Filepath))
-   }
-   CheckTranslation('/Users/lucascdz/FILES/atomiclab/corpora/UD_Latin-PROIEL/VulgataMatt_translAlignment.tsv')
+   return(CheckTranslationDF)
 
 }
--- R/translatinumCor_pipeline.R
+++ R/translatinumCor_pipeline.R
@@ -4,18 +4,28 @@ library(antiword)
 ## ARGUMENTS 1
 conlluFile <- '../../FILES/atomiclab/corpora/UD_Latin-PROIEL/Cicero_Epistulae_ad_Atticum.conllu'
 corpusname <- 'proiel'
-docfile <- './data/translations/Cic-b06-Ad Att-LIBER VI (feito).doc'
+docfile <- './data/0_translations/Cic-b06-Ad Att-LIBER VI (feito).doc'
+mdfile <- './data/0_translations/Cic-b06-Ad Att-LIBER VI (feito).doc.md'
+translator <- 'José Dejalma Dezotti'
 
 ## FUNCTIONS
 source('./R/translatinumCor_functions.R')
 
 ## GET sentences already aligned
-TranslatinumCorDF <- read.delim2('./translatinumCor-pt_v.1.0.tsv')
+#TranslatinumCorDF <- read.delim2('./translatinumCor-pt_v.1.0.tsv')
+#TranslatinumCorDF$translator <- 'José Dejalma Dezotti'
+#TranslatinumCorDF$sameLatinText <- T
+#TranslatinumCorDF$sameLatinText[TranslatinumCorDF$DIFF!=''] <- F
+#TranslatinumCorDF <- TranslatinumCorDF[,colnames(TranslatinumCorDF)!='DIFF']
+#colnames(TranslatinumCorDF)[1:2] <- c('sentence_id','translation')
+#TranslatinumCorDF$sentence_id <- gsub('proiel_','',TranslatinumCorDF$sentence_id)
+#write_tsv(TranslatinumCorDF,'./translatinumCor-pt_v.1.1.tsv')
+TranslatinumCorDF <- read.delim2('./translatinumCor-pt_v.1.1.tsv')
 
 ## GET sentence metadata
 # NB: needs 'SentMetadata_corpusname.tsv' in data folder
 SentMetadataDF <- GetSentenceMetadata(corpusname)
-SentMetadataNewDF <- SentMetadataDF[!SentMetadataDF$sent_id %in% TranslatinumCorDF$sent_id,]
+SentMetadataNewDF <- SentMetadataDF[!SentMetadataDF$sentence_id %in% TranslatinumCorDF$sentence_id,]
 
 ## ARGUMENTS 2
 unique(SentMetadataDF$filename)
@@ -27,13 +37,16 @@ refstring <- 'Cic.Att.6'
 SentencesDF <- GetSentencesFromConllu(conlluFile,corpusname,refstring,SentMetadataDF)
 
 ## GET translations from doc file
-TextTranslationDF <- GetTranslationsFromDOC(docfile,SentencesDF,refstring)
+TextTranslationDF <- GetTranslationsFromMD(mdfile,corpusname,refstring,translator)
 
 ########################################
-#  DO TRANSLATION ALIGNMENT MANUALLY   #
-# join SentencesDF | TextTranslationDF #
+#   DO TRANSLATION ALIGNMENT MANUALLY  #
+# sentences + align2check = align4diff #
 ########################################
 
+alignedfile <- paste0('./data/2_align4diff/',gsub('\\.','_',refstring),'_',corpusname,'_align4diff.tsv')
 
+## GET final alignment
+CheckTranslationDF <- CheckTranslationDiff(alignedfile,refstring,corpusname)
 
 
