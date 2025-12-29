1cd3834ea18040f3c2895729b9ce081f87865494
Lucas Dezotti <lucascdz@gmail.com>
2025-12-29 00:39:49 -0300

    working (filename based)

--- R/translatinumCor_tempGetLaslaCorpus.R
+++ R/translatinumCor_tempGetLaslaCorpus.R
@@ -1,15 +1,24 @@
 
-
+library(stringr)
 library(readtext)
 
-test <- readtext::readtext('/Users/lucascdz/FILES/atomiclab/corpora/DezottiJD_traducoes/TRADUCOES_LASLA_Corpus/Cicero_InVerremActioPrima_Cic1Ver-FEITO.docx')
+test <- readtext::readtext('/Users/lucascdz/FILES/atomiclab/corpora/DezottiJD_traducoes/TRADUCOES_LASLA_Corpus/Caesar_BellumCivile_CaesBC2.docx')
 test2 <- readLines(textConnection(test$text))
-head(test2)
-test2 <- test2[which(test2=='sent_id'):length(test2)]
 
-DataFrame <- data.frame(sentids=test2[seq(5,length(test2),by=4)],
-                        latintext=test2[seq(7,length(test2),by=4)],
-                        translation=test2[seq(8,length(test2),by=4)],
+filename <- 'Caesar_BellumCivile_CaesBC2.docx'
+indices_start <- which(str_detect(test2,gsub('.*_(\\w+).docx','\\1',filename)))
+indices_stop <- indices_start-1
+indices_stop <- c(indices_stop[2:length(indices_stop)],length(test2))
+
+testLIST <- lapply(seq_along(indices_start),
+                   function(i) c(test2[indices_start[i]:indices_stop[i]]))
+
+DataFrame <- data.frame(sentids=unlist(lapply(seq_along(testLIST),
+                                              function(i) testLIST[[i]][1])),
+                        latintext=unlist(lapply(seq_along(testLIST),
+                                                function(i) testLIST[[i]][3])),
+                        translation=unlist(lapply(seq_along(testLIST),
+                                                  function(i) testLIST[[i]][4])),
                         stringsAsFactors = F)
 
-
+DataFrame <- DataFrame[!is.na(DataFrame$translation),]
